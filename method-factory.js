0d6d81ba268980e880a273cc452da9916efecfa9
jankapunkt <jkuester@uni-bremen.de>
2023-10-17 11:43:04 +0200

    fix: add full options object to schema factory

--- method-factory.js
+++ method-factory.js
@@ -52,7 +52,7 @@ export const createMethodFactory = ({ custom, mixins, schemaFactory } = {}) => {
 
     let validateFn = validate
     if (!validateFn && schemaFactory) {
-      const validationSchema = schemaFactory(schema)
+      const validationSchema = schemaFactory(schema, options)
       // we fallback to a plain object to support Meteor.call(name, callback)
       // for schemas that contain no property: { schema: {} }
       validateFn = function validate (document = {}) {
