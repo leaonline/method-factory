7b3bf7fe2978465310e6100a57e3d12a38d32e64
jankapunkt <jkuester@uni-bremen.de>
2024-07-17 11:56:31 +0200

    deps: upgrade to Meteor 3.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* eslint-env meteor */
 Package.describe({
   name: 'leaonline:method-factory',
-  version: '1.1.0',
+  version: '2.0.0',
   // Brief, one-line summary of the package.
   summary: 'Create validated Meteor methods. Lightweight. Simple.',
   // URL to the Git repository containing the source code for this package.
@@ -12,23 +12,19 @@ Package.describe({
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.6')
+  api.versionsFrom(['3.0.1'])
   api.use('ecmascript', 'server')
-  api.use('mdg:validated-method@1.2.0', 'server')
+  api.use('mdg:validated-method@1.3.0', 'server')
   api.mainModule('method-factory.js', 'server')
 })
 
 Package.onTest(function (api) {
-  Npm.depends({
-    chai: '4.2.0',
-    'simpl-schema': '1.6.2'
-  })
-
+  api.versionsFrom(['3.0.1'])
   api.use('ecmascript')
   api.use('mongo')
   api.use('random')
-  api.use('mdg:validated-method@1.2.0')
-  api.use('meteortesting:mocha')
+  api.use('mdg:validated-method@1.3.0')
+  api.use('meteortesting:mocha@3.0.0-rc.1')
   api.use('leaonline:method-factory')
   api.mainModule('method-factory-tests.js')
 })
