66dfa0de263bb1fc58916e3a87f3944f8c0934d2
jankapunkt <jkuester@uni-bremen.de>
2025-01-13 14:17:45 +0100

    migration: make meteor 3 compatible and add schema options to factory

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* eslint-env meteor */
 Package.describe({
   name: 'leaonline:method-factory',
-  version: '1.1.0',
+  version: '2.0.0',
   // Brief, one-line summary of the package.
   summary: 'Create validated Meteor methods. Lightweight. Simple.',
   // URL to the Git repository containing the source code for this package.
@@ -12,23 +12,23 @@ Package.describe({
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.6')
+  api.versionsFrom(['1.6', '2.8.0', '3.0.1'])
   api.use('ecmascript', 'server')
-  api.use('mdg:validated-method@1.2.0', 'server')
+  api.use('mdg:validated-method@1.3.0', 'server')
   api.mainModule('method-factory.js', 'server')
 })
 
 Package.onTest(function (api) {
   Npm.depends({
     chai: '4.2.0',
-    'simpl-schema': '1.6.2'
+    'simpl-schema': '1.13.1'
   })
 
   api.use('ecmascript')
   api.use('mongo')
   api.use('random')
-  api.use('mdg:validated-method@1.2.0')
-  api.use('meteortesting:mocha')
+  api.use('mdg:validated-method@1.3.0')
+  api.use('meteortesting:mocha@2.1.0 || 3.2.0')
   api.use('leaonline:method-factory')
   api.mainModule('method-factory-tests.js')
 })
